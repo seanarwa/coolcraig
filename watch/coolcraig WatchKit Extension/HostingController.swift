398297235981de486906dfe05a2d9f38cd19d124
Sean Chang <seanarwa@gmail.com>
2019-10-10 17:02:41 -0700

    Added iOS watch and phone project files

---
+++ watch/coolcraig WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  coolcraig WatchKit Extension
+//
+//  Created by MRC on 10/10/19.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ watch/coolcraig WatchKit Extension/NotificationView.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationView.swift
+//  coolcraig WatchKit Extension
+//
+//  Created by MRC on 10/10/19.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
