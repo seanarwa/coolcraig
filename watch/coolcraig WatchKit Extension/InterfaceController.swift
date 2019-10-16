d70d001b870983353c167342f877701531ea92fa
Sean Chang <seanarwa@gmail.com>
2019-10-15 18:12:44 -0700

    Added basic code for task list

---
+++ watch/coolcraig WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceController.swift
+//  coolcraig WatchKit Extension
+//
+//  Created by MRC on 10/15/19.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+    
+    
+    
+    
+
+}
