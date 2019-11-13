2fd12fb2af2e7bcd1f6e5853e3498d65042a0ea8
Sean Chang <seanarwa@gmail.com>
2019-11-13 14:20:16 -0800

    Modified xcode settings to use Config.xcconfig

---
+++ watch/coolcraig WatchKit Extension/ProfileInterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileInterfaceController.swift
+//  coolcraig WatchKit Extension
+//
+//  Created by InfProjCourse1 on 11/13/19.
+//
+
+import WatchKit
+import Foundation
+
+
+class ProfileInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
